22ab4b692f6e4932717b561efa4e41ab8b55263c
Bright <airxqhobarbryt@gmail.com>
2026-05-23 00:23:30 +0000

    Loading bug fix

--- vite.config.js
+++ vite.config.js
@@ -10,8 +10,12 @@ export default defineConfig({
     },
   },
   server: {
+    host: true,
     port: 5173,
     strictPort: false,
+    watch: {
+      usePolling: true,
+    },
   },
   preview: {
     port: 4173,
